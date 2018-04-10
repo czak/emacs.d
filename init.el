391d38aeea0ada19d91957df154a53cd99267b71
Łukasz Adamczak <lukasz@czak.pl>
2018-04-10 17:46:06 +0200

    Simplify magit bind

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 
 (use-package magit
   :ensure t
-  :bind (("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
   :config
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
